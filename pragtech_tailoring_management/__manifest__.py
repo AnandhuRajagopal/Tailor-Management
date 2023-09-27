b2829f976d95082621109b730e1bb42ad06d2462
asmarrahman <asmar.t@pragtech.co.in>
2023-09-27 17:45:38 +0530

    [cloth type model and view added]

--- pragtech_tailoring_management/__manifest__.py
+++ pragtech_tailoring_management/__manifest__.py
@@ -24,9 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/view_cloth_type.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
