a85f580f5780d8b1202fb1cbaea39ec5189af07b
AnandhuRajagopal <anandhu.nair@pragtech.co.in>
2023-09-29 13:38:34 +0530

    Added main menu

--- pragtech_tailoring_management/wizard/assigning_tailor_wizard.py
+++ pragtech_tailoring_management/wizard/assigning_tailor_wizard.py
@@ -2,8 +2,8 @@ from odoo import models, fields, api
 
 
 class assigningTailorWizard(models.TransientModel):
-    _name = 'tailoring.wizard'
-    _description = 'tailoring_wizard'
+    _name = 'tailoring.assign.tailors'
+    _description = 'tailoring_assign_tailors'
 
 
     tailor_id = fields.Char(string='Select Tailor')
