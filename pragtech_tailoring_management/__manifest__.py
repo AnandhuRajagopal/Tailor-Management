99d250bf69af8cc89f418831f3a2286eb2cb9d92
RafiPragtech <mohammed.rafi@pragtech.co.in>
2023-09-29 10:19:54 +0530

    added sequence for measurments

--- pragtech_tailoring_management/__manifest__.py
+++ pragtech_tailoring_management/__manifest__.py
@@ -25,12 +25,11 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        'views/menu.xml',
         'wizard/assigning_tailor_wizard.xml',
         'views/view_cloth_type.xml',
+        'views/sequence_measurment.xml',
         'views/view_measurement.xml',
+        'views/menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
