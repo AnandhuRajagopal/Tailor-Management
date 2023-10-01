436e59a8a375ba90e93ac7006218b93ac764e988
asmarrahman <asmar.t@pragtech.co.in>
2023-10-01 19:45:14 +0530

    security group added

--- pragtech_tailoring_management/models/fabrics.py
+++ pragtech_tailoring_management/models/fabrics.py
@@ -3,7 +3,7 @@ from odoo import fields,models,api
 
 class TailoringFabric (models.Model):
     _name = "tailoring.fabric"
-    _description = "tailoring.fabric"
+    _description = "tailoring_fabric"
 
     name = fields.Char('Name of Fabric')
 
