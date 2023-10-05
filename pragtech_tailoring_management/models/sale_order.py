30b0d27e27d3ec1dc97b3b98fb063d6a4835ef11
asmarrahman <asmar.t@pragtech.co.in>
2023-10-05 18:48:18 +0530

    ...

--- pragtech_tailoring_management/models/sale_order.py
+++ pragtech_tailoring_management/models/sale_order.py
@@ -28,28 +28,28 @@ class SaleOrder(models.Model):
 
 
 
-    def measurement(self):
-        print("measurement")
+    # def measurement(self):
+    #     print("measurement")
 
 
-    def action_print(self):
-        print(".....")
+    # def action_print(self):
+    #     print(".....")
 
 
 
 
 
-    def action_send_mail(self):
-        active_id = self.env.context.get('active_id')
-        sale_order = self.env['sale.order'].browse(active_id)
-        print("---------------active model--------------------", active_id)
-        email_values = {
-        'email_from': self.company_id.email,
-        'email_to': self.partner_id.email,
-        'subject': 'Assigned Product Details'
-        }
-        template = self.env.ref('pragtech_tailoring_management.mail_template_ready_to_delivery')
-        template.send_mail(sale_order.id, force_send=True, email_values=email_values)
+    # def action_send_mail(self):
+    #     active_id = self.env.context.get('active_id')
+    #     sale_order = self.env['sale.order'].browse(active_id)
+    #     print("---------------active model--------------------", active_id)
+    #     email_values = {
+    #     'email_from': self.company_id.email,
+    #     'email_to': self.partner_id.email,
+    #     'subject': 'Assigned Product Details'
+    #     }
+    #     template = self.env.ref('pragtech_tailoring_management.mail_template_ready_to_delivery')
+    #     template.send_mail(sale_order.id, force_send=True, email_values=email_values)
 
 
 
