7bf0905eaeeaa45532267b73ed316f40bddb1ead
Razeen <razeen.abdurahman@pragtech.co.in>
2023-10-10 15:08:17 +0530

    Done the connection of measurement from front end to backend and viceversa and also added state field in customer measurement model

--- pragtech_tailoring_management/__manifest__.py
+++ pragtech_tailoring_management/__manifest__.py
@@ -56,7 +56,6 @@
         'web.assets_frontend': [
             'pragtech_tailoring_management/static/src/css/home.css',
             'pragtech_tailoring_management/static/src/js/measurement.js',
-            'https://code.jquery.com/jquery-3.6.0.min.js'
         ]
     },
 }
